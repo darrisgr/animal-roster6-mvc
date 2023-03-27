84f271f60b04a7b08916f770f147a71549f3f2c6
Gerard Darris <darrisgr@gmail.com>
2023-03-27 15:24:01 -0500

    Finished lecture

---
+++ AnimalRoster6/Models/Animal.cs
@@ -0,0 +1,46 @@
+﻿using System;
+namespace AnimalRoster6.Models
+{
+	public class Animal
+	{
+
+        public string Name { get; set; }
+		public string Species { get; set; }
+		public string Description { get; set; }
+		public string ImgUrl { get; set; }
+        private static int nextId = 0;
+        public int Id { get; }
+
+
+        public Animal()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Animal(string name, string species, string description, string imgUrl)
+        {
+            Name = name;
+            Species = species;
+            Description = description;
+            ImgUrl = imgUrl;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Animal animal &&
+                   Id == animal.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+
+        public override string? ToString()
+        {
+            return Name;
+        }
+    }
+}
+
